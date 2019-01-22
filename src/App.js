bd3334a0be844a77f8fe0961f323fa1c3ae9700f
Hani <msowe.feha@gmail.com>
2019-01-22 22:18:31 +0300

    Manipulator UI ready

--- src/App.js
+++ src/App.js
@@ -4,21 +4,55 @@ import Manipulator from './Components/Manipulator';
 import Generator from './Components/Generator';
 import MyDocs from './Components/MyDocs';
 
-const allTabs = ["Manipulate text", "Lorem ipsum Generator", "My Docs"];
+const allTabs = ["Manipulate text", "Lorem ipsum Generator", "Language detector", "My Docs"];
 
 export default function App() {
   const [tabs] = useState(allTabs);
   const [activeTab, setActiveTab] = useState("Manipulate text");
   const [manipulatorText, setManipulatorText] = useState();
-  const [generatorText, setGeneratorText] = useState();
+  const [stringCounts, setStringCounts] = useState({ words: 0, chars: 0 });
 
-  const getComponent = (tab) => {
+
+  function handleManipulatorText(text) {
+    setManipulatorText(text);
+    setStringCounts({ words: countWords(text), chars: countChars(text) });
+  }
+
+
+  function getComponent(tab) {
     switch (tab) {
-      case "Manipulate text": return <Manipulator text={manipulatorText} handleChangeText={setManipulatorText}/>
+      case "Manipulate text":
+      return (
+        <Manipulator
+          text={manipulatorText}
+          handleChangeText={handleManipulatorText}
+          stringCounts={stringCounts}
+          />
+      )
       case "Lorem ipsum Generator": return <Generator />
       case "My Docs": return <MyDocs />
       default:
     }
+  }
+
+  function countWords(text = ''){
+    if (text.trim()) {
+      let ar = 0, en = 0;
+      if (text.match(/\S/g)) {
+        //Arabic words
+        let pta = /[\u0600-\u06FF]+/g
+        if (pta.test(text)) { ar = text.match(pta).length }
+        //English words
+        let pte = /\w+/g
+        if (pte.test(text)) { en = text.match(pte).length }
+        return ar + en
+      } else { return 0 }
+    }  else { return 0 }
+  }
+
+
+  function countChars(text){
+    return text ? text.length : 0;
   }
 
   const handleClickTab = (activeTab) => setActiveTab(activeTab);
@@ -31,6 +65,16 @@ export default function App() {
           {getComponent(activeTab)}
         </div>
       </div>
+
+      <hr />
+
+      <div className="text-center mt-4 mb-5">
+          Textmani 2019
+        <a href="https://github.com/ihani/textmani-react">
+          <span> <i class="fab fa-github"></i></span>
+        </a>
+      </div>
+
     </div>
   );
 }
--- src/Components/Manipulator.js
+++ src/Components/Manipulator.js
@@ -1,96 +1,185 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 export default function Manipulator(props) {
-  const {text, handleChangeText} = props;
-  const [statusMessage, setStatusMessage] = useState('Type or paste text above to manipulate');
-  const [words, setWords] = useState(0);
-  const [chars, setChars] = useState(0);
-  const textarea = React.createRef();
+  const {text = '', handleChangeText, stringCounts, countMatchesString, setCountMatchesString} = props;
+  const [statusMessage, setStatusMessage] = useState('Ready');
+  // const textarea = React.createRef();
+  const textarea = useRef();
 
   useEffect(() => {
-    textarea.current.focus();
-  });
+    if (!text.trim()) {
+      handleChangeText('');
+      setStatusMessage('Ready');
+
+    }
+  }, [text]);
 
   function clearText() {
     handleChangeText('');
-    setWords(0);
-    setChars(0);
+    textarea.current.focus();
   }
 
-  function countWords(text){
-    if (text.trim()) {
-      let ar = 0, en = 0;
-      if (text.match(/\S/g)) {
-        //Arabic words
-        let pta = /[\u0600-\u06FF]+/g
-        if (pta.test(text)) { ar = text.match(pta).length }
-        //English words
-        let pte = /\w+/g
-        if (pte.test(text)) { en = text.match(pte).length }
-        return ar + en
-      } else { return 0 }
-    }  else { return 0 }
-  }
-
-  function countChars(text){
-    return text.length;
-  }
-
-  const handleOnChange = (e) => {
+  function handleOnChange(e) {
     const text = e.target.value;
     handleChangeText(text);
-    setWords(countWords(text));
-    setChars(countChars(text));
+    setStatusMessage('Ready');
+  }
+
+  function handleMatchingCounter(text) {
+    // console.log(handleMatchingCounter)
+    const found = 55;
+    setStatusMessage(`Found ${found} matches.`);
+
   }
 
   return (
     <div>
-      <div className="form-group">
-        <textarea
-          value={text}
-          onChange={handleOnChange}
-          className="form-control"
-          ref={textarea}
-          rows="5"
-          placeholder="..."
-          autoFocus>
-        </textarea>
-      </div>
-
-      <div className="container">
+      <div className="container mb-2">
         <div className="row">
-
           <div className="col-3">
             <div className="row">
               <div className="col-sm-6">
                 <h6>
-                  words: {words}
+                  words: {stringCounts.words}
                 </h6>
               </div>
               <div className="col-sm-6">
                 <h6>
-                  chars: {chars}
+                  chars: {stringCounts.chars}
                 </h6>
               </div>
             </div>
           </div>
-
           <div className="col-6 text-center dotted">
             {statusMessage}
           </div>
-
           <div className="col-3 text-right">
             <div className="btn-group" role="group" aria-label="Clear | Copy">
               <button
                 type="button"
                 className="btn btn-secondary"
                 onClick={clearText}
+                value={countMatchesString}
                 >Clear
               </button>
               <button type="button" className="btn btn-secondary">Copy</button>
             </div>
           </div>
+        </div>
+      </div>
 
+      <div className="form-group">
+        <textarea
+          value={text}
+          onChange={handleOnChange}
+          className="form-control"
+          ref={textarea}
+          rows="4"
+          placeholder="Type or paste text here to manipulate"
+          autoFocus>
+        </textarea>
+      </div>
+
+      <div className="container section">
+        <div className="row">
+          <div className="col-3 text-right">
+            Change case
+          </div>
+          <div className="col">
+            <div className="btn-group" role="group" aria-label="Basic example">
+              <button type="button" className="btn btn-primary">lowercase</button>
+              <button type="button" className="btn btn-primary">UPPERCASE</button>
+              <button type="button" className="btn btn-primary">Capitalized Case</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <hr />
+
+      <div className="container section">
+        <div className="row">
+          <div className="col-3 text-right">
+            Replace
+          </div>
+          <div className="col-4 pb-1">
+            <input type="text" className="form-control" aria-describedby="replace text"
+              onChange={e => handleMatchingCounter(e.target.value)} />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-3 text-right">
+            with
+          </div>
+          <div className="col-4">
+            <input type="text" className="form-control" aria-describedby="replace with"
+              onChange={e => handleMatchingCounter(e.target.value)} />
+          </div>
+          <div className="col-1 text-right">
+            <div className="text-left" aria-label="Replace">
+              <button type="button" className="btn btn-warning">Replace</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <hr />
+
+      <div className="container section">
+        <div className="row">
+          <div className="col-3 text-right">
+            Remove
+          </div>
+          <div className="col-4 pb-1">
+            <input type="text" className="form-control" aria-describedby="replace text"
+              onChange={e => handleMatchingCounter(e.target.value)} />
+          </div>
+          <div className="col">
+            <div className="btn-group" role="group" aria-label="Basic example">
+              <button type="button" className="btn btn-warning">remove</button>
+              <button type="button" className="btn btn-warning">ازالة المـد</button>
+              <button type="button" className="btn btn-warning">إزالة التشكيل</button>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+      <hr />
+
+      <div className="container section">
+        <div className="row">
+          <div className="col-3 text-right">
+            Count matches for
+          </div>
+          <div className="col-4">
+            <input type="text" className="form-control" aria-describedby="count words"
+              onChange={e => handleMatchingCounter(e.target.value)} />
+          </div>
+          <div className="col text-left">
+            <div class="alert alert-success m-0 p-2" role="alert">
+              Found 54 matches
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <hr />
+
+      <div className="container section">
+        <div className="row">
+          <div className="col-3 text-right">
+            Scramble text
+          </div>
+
+          <div className="col">
+            <div className="btn-group" role="group" aria-label="Basic example">
+              <button type="button" className="btn btn-danger">Scramble letters</button>
+              <button type="button" className="btn btn-danger">Scramble words</button>
+              <button type="button" className="btn btn-danger">Scramble sentences</button>
+            </div>
+          </div>
         </div>
       </div>
 
