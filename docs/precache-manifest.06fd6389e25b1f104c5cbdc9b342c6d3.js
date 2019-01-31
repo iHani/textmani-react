4f87ffb53a273f31bc4fe6451856bcc288e3ad65
Hani <msowe.feha@gmail.com>
2019-01-31 10:04:07 +0300

    docs build included

---
+++ docs/precache-manifest.06fd6389e25b1f104c5cbdc9b342c6d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2effefe61c2b6d90226a",
+    "url": "/static/js/main.2effefe6.chunk.js"
+  },
+  {
+    "revision": "5360832ef9d50428dca5",
+    "url": "/static/js/1.5360832e.chunk.js"
+  },
+  {
+    "revision": "2effefe61c2b6d90226a",
+    "url": "/static/css/main.08dc48e8.chunk.css"
+  },
+  {
+    "revision": "5360832ef9d50428dca5",
+    "url": "/static/css/1.1f1bf88f.chunk.css"
+  },
+  {
+    "revision": "5e0bc14f608c5a8347abb7861d5eac6a",
+    "url": "/index.html"
+  }
+];
